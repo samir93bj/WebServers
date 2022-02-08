a571c1dab50ab01172d7f49c1e0d33c5a75e6930
samir93bj <samir93bj@gmail.com>
2022-02-07 22:54:54 -0300

    Pruebas de HTTP

---
+++ app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const { Http2ServerRequest } = require('http2');
+const { listenerCount } = require('process');
+
+http.createServer((req, res) => {
+
+    /* res.writeHead(200, {'Content-Type': 'application/json'});
+
+    persona = {
+        id:1,
+        nombre : 'Samir'
+    }
+
+    res.write(JSON.stringify(persona));*/
+
+
+    res.setHeader('content-disposition', 'attachment; filename="lista.csv"');
+    res.writeHead(200, {'Content-Type': 'application/csv'});
+
+    res.write('id, nombre \n');
+    res.write('1, Fernando \n');
+    res.write('2, Carlos \n');
+    res.write('3, Samir   \n');
+    res.write('4, Flor \n');
+    
+    res.end;
+})
+.listen(8080);
+
+console.log('Escuchando en el puerto', 8080);
