462cc645a845be15a8e262c913b8814bc1b8b613
samir93bj <samir93bj@gmail.com>
2022-02-12 00:58:46 -0300

    Partials whit HBS. OK

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-const express = require('express')
+const express = require('express');
+
 const app = express();
 const port = 8080;
+const hbs = require('hbs');
 
-//
+//Handlebars
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials'); //hbs
 
-//Middleware
 // Servir contenido estatico
 app.use(express.static('public'));
 
 
 //De esta manera podemos renderizar la view del index
 app.get('/',  (req, res) => {
-  res.render('home');
+  res.render('home', {
+    nombre : 'Samir Mahmud',
+    titulo: 'Curso de Node JS'
+  });
 });
 
 app.get('/generic',  (req, res) => {
-  res.sendFile(__dirname + '/public/generic.html');
+    res.render('generic', {
+    nombre : 'Samir Mahmud',
+    titulo: 'Curso de Node JS'
+  });
 });
 
 app.get('/elements',  (req, res) => {
-  res.sendFile(__dirname + '/public/elements.html');
+    res.render('elements', {
+    nombre : 'Samir Mahmud',
+    titulo: 'Curso de Node JS'
+  });
 });
 
 app.get('*',  (req, res) => {
